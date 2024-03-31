2f932d0da335198dba6be37b0a32c92b741f4a41
Mahmud Hasan <mhasan015@citymail.cuny.edu>
2024-03-31 21:17:02 +0000

    added fetching bus info from database and updating the frontend map as well as adding new metric for branch info

--- metric_scripts/generate_metrics_report.py
+++ metric_scripts/generate_metrics_report.py
@@ -1,15 +1,12 @@
 from github import Github
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_pdf import PdfPages
 import os
 import numpy as np
 from comments_data import comments_data
 from pr_duration import pr_duration
 from reviews_per_contributor import reviews_per_contributor
 from commits_per_pr import commits_per_pr
-
-g = Github(os.getenv('GITHUB_TOKEN'))
-repo = g.get_repo("mhshabbir/ccny-bus-track")
+from branch_info import branch_commits_info
 
 def generate_metrics_report():
     pull_requests = list(repo.get_pulls(state='all', sort='created', base='main'))
@@ -17,6 +14,9 @@ def generate_metrics_report():
     commits_per_pr(pull_requests)
     pr_duration(pull_requests)
     reviews_per_contributor(pull_requests)
+    branch_commits_info(repo)
 
 if __name__ == "__main__":
+    g = Github(os.getenv('GITHUB_TOKEN'))
+    repo = g.get_repo("mhshabbir/ccny-bus-track")
     generate_metrics_report()
