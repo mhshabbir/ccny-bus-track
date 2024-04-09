fc6220f47950715946ed414fd17063f8f41c9f4e
Mahmud Hasan <mhasan015@citymail.cuny.edu>
2024-04-09 09:07:01 +0000

    changed distance measure to euclidian from manhattan

--- getDistance.js
+++ getDistance.js
@@ -1,13 +1,6 @@
-function calculateManhattanDistance(coord1, coord2) {
-    console.log(coord1[0], coord2);
-
-    // Calculate Manhattan distance (L1 distance)
-    return Math.abs(coord1[0] - coord2[0]) + Math.abs(coord1[1] - coord2[1]);
-}
-
+// Calculate Euclidean distance (L2 distance)
 function calculateEuclideanDistance(coord1, coord2) {
-    // Calculate Euclidean distance (L2 distance)
-    return Math.sqrt((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2);
+    return Math.sqrt(Math.pow(coord1[0] - coord2[0], 2) + Math.pow(coord1[1] - coord2[1], 2));
 }
 
-module.exports = { calculateEuclideanDistance, calculateManhattanDistance }
+module.exports = { calculateEuclideanDistance }
