d0be92b4fd15ff22cb19180d2ed17eb997f76eb2
Mahmud Hasan <mhasan015@citymail.cuny.edu>
2024-04-09 03:46:22 +0000

    added export import for at stop function

--- atStop.js
+++ atStop.js
@@ -1,3 +1,5 @@
+const getDistance = require('./getDistance')
+
 function atStop(current_coord) {
     const schoolCoord = [-73.949775, 40.819798];
     const station125 = [-73.945460, 40.807812];
@@ -6,9 +8,9 @@ function atStop(current_coord) {
     const radius = .000245;
 
     // Calculate distances to each stop
-    const distanceToSchool = calculateEuclideanDistance(current_coord, schoolCoord);
-    const distanceToStation125 = calculateEuclideanDistance(current_coord, station125);
-    const distanceToStation145 = calculateEuclideanDistance(current_coord, station145);
+    const distanceToSchool = getDistance.calculateEuclideanDistance(current_coord, schoolCoord);
+    const distanceToStation125 = getDistance.calculateEuclideanDistance(current_coord, station125);
+    const distanceToStation145 = getDistance.calculateEuclideanDistance(current_coord, station145);
 
     // Check if current coordinate is within radius of each stop
     if (distanceToSchool <= radius) {
@@ -21,3 +23,5 @@ function atStop(current_coord) {
         return false;
     }
 }
+
+module.exports = { atStop };
