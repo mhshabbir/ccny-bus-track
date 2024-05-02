099c6d71a82202abea63d141264585fd847dd8e0
Mahmud Hasan <mhasan015@citymail.cuny.edu>
2024-05-02 03:02:21 +0000

    terraform codespaces, with terraform automated infrastructure provisioning

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -3,10 +3,38 @@ resource "aws_elastic_beanstalk_application" "my_app" {
   description = "ccny bustrack"
 }
 
+resource "aws_iam_instance_profile" "eb_instance_profile" {
+  name = "eb-instance-profile"
+  role = aws_iam_role.eb_role.name
+}
+
+resource "aws_iam_role" "eb_role" {
+  name = "eb-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Effect = "Allow"
+      },
+    ]
+  })
+}
+
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = "bustrack-env"
   application         = aws_elastic_beanstalk_application.my_app.name
-  solution_stack_name = "64bit Amazon Linux 2 v5.2.3 running Node.js 12"
+  solution_stack_name = "64bit Amazon Linux 2023 v6.1.4 running Node.js 20"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "IamInstanceProfile"
+    value     = aws_iam_instance_profile.eb_instance_profile.name
+  }
 
   setting {
     namespace = "aws:autoscaling:asg"
