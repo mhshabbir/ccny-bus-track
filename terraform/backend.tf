dd52d7d2e78dc862c35f163ac871c46750fd141d
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-05-06 18:03:15 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-state-ccnybustracker-mshabbir76"
-    key    = "core/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
