7cd8e24480315341c409ea62622fafb144c3f65c
Muhammad Hasnain Shabbir <83360044+mhshabbir@users.noreply.github.com>
2024-05-06 18:03:49 -0400

    Update elastic_beanstalk.tf

--- terraform/elastic_beanstalk.tf
+++ terraform/elastic_beanstalk.tf
@@ -1,9 +1,9 @@
 resource "aws_elastic_beanstalk_application" "application" {
-  name = "flaskbb"
+  name = "ccnybustracker"
 }
 
 resource "aws_elastic_beanstalk_environment" "environment" {
-  name                = "flaskbb-environment"
+  name                = "ccnybustracker-environment"
   cname_prefix        = "mshabbir76"
   application         = aws_elastic_beanstalk_application.application.name
   solution_stack_name = "64bit Amazon Linux 2023 v4.0.1 running Python 3.11"
