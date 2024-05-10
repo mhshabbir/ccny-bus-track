a036422de6327f60bcbc1ab09a16522d2478ba1e
Mahmud Hasan <mhasan015@citymail.cuny.edu>
2024-05-10 12:15:31 +0000

    created simulator and updated waitTime+atStop functions to match new bus data structure

--- server/utils/test/test_getWaitTime.js
+++ server/utils/test/test_getWaitTime.js
@@ -1,28 +1,24 @@
 const busRoutes = require('../busRoutes.js');
 const getWaitTime = require('../getWaitTime.js');
 
-
-// test bus
-// const bus = {
-//     id: 1,
-//     currentCoord: busRoutes.route_CCNY_145[3],
-//     nextStop: '145',
-//     previousStop: 'CCNY'
-// };
-
-const bus = {
-    id: 1,
-    currentCoord: busRoutes.route_CCNY_145[0], // at ccny stop
-    nextStop: '145',
-    previousStop: 'CCNY'
-}; // shows waittime for 145 
-
-// const bus = {
-//     id: 1,
-//     currentCoord: [ -73.94545, 40.822777 ],
-//     nextStop: '145',
-//     previousStop: 'CCNY'
-// };
+// Define all bus objects in an array
+const buses = [
+    {
+        id: 1,
+        location: busRoutes.route_CCNY_145[0], // at ccny stop
+        stop: {next:'145', prev:'CCNY'}
+    },
+    {
+        id: 1,
+        location: busRoutes.route_CCNY_145[3], // on the way to 145th
+        stop: {next: '145', prev: 'CCNY'} 
+    },
+    {
+        id: 1,
+        location: [-73.94545, 40.822777], // at 145th
+        stop: {next: '145', prev: 'CCNY'} 
+    }
+];
 
 // Test function
 function test_getWaitTime(bus) {
@@ -30,5 +26,7 @@ function test_getWaitTime(bus) {
     console.log(`The wait times are: CCNY - ${waitTimeCCNY}, 125th Station - ${waitTime125}, 145th Station - ${waitTime145}`);
 };
 
-// Run the test
-test_getWaitTime(bus);
+// Run the test for each bus
+for (const bus of buses) {
+    test_getWaitTime(bus);
+}
