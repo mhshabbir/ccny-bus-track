d4629321aaae6bf6dad707938887a8e1e64db948
Mahmud Hasan <mhasan015@citymail.cuny.edu>
2024-05-10 11:12:55 +0000

    added asserts to mongodb tests

--- server/utils/test/test_updateMongo.js
+++ server/utils/test/test_updateMongo.js
@@ -1,36 +1,52 @@
 // test_updateMongo.js
 
-const { updateLocation, updateNextStop, updatePrevStop, readBusData } = require('../busMongo.js');
+const { updateLocation, updateStop, updateTime, readBusData } = require('../busMongo.js');
+const assert = require('assert');
+
 
 async function testUpdateLocation() {
     const busId = "1";
     const newLat = -74.005974;
     const newLng = 40.712776;
+
     await updateLocation(busId, newLat, newLng);
     const updatedBusData = await readBusData(busId);
-    console.log('Updated Location:', updatedBusData.location);
+
+    assert.strictEqual(updatedBusData.document.location.lat, newLat, "Latitude did not update correctly");
+    assert.strictEqual(updatedBusData.document.location.lng, newLng, "Longitude did not update correctly");
 }
 
-async function testUpdateNextStop() {
+async function testUpdateStop() {
     const busId = "1";
-    const nextStop = "CCNY";
-    await updateNextStop(busId, nextStop);
+    const nextStop = "145";
+    const prevStop = "CCNY";
+
+    await updateStop(busId, nextStop, prevStop);
     const updatedBusData = await readBusData(busId);
-    console.log('Updated Next Stop:', updatedBusData.nextStop);
+
+    assert.strictEqual(updatedBusData.document.stop.next, nextStop, "Next stop did not update correctly");
+    assert.strictEqual(updatedBusData.document.stop.prev, prevStop, "Previous stop did not update correctly");
 }
 
-async function testUpdatePrevStop() {
+async function testUpdateTime() {
     const busId = "1";
-    const prevStop = "145";
-    await updatePrevStop(busId, prevStop);
+    const ccny = 5;
+    const uptown = 10; 
+    const downtown = 8; 
+
+    await updateTime(busId, ccny, uptown, downtown);
     const updatedBusData = await readBusData(busId);
-    console.log('Updated Previous Stop:', updatedBusData.prevStop);
+
+    assert.strictEqual(updatedBusData.document.wait.ccny, ccny, "CCNY wait time did not update correctly");
+    assert.strictEqual(updatedBusData.document.wait.uptown, uptown, "Uptown wait time did not update correctly");
+    assert.strictEqual(updatedBusData.document.wait.downtown, downtown, "Downtown wait time did not update correctly");
+
 }
 
 async function runTests() {
     await testUpdateLocation();
-    await testUpdateNextStop();
-    await testUpdatePrevStop();
+    await testUpdateStop();
+    await testUpdateTime(); 
 }
 
 runTests();
